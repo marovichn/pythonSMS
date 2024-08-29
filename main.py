24aafbc8a8833f5d77c25e7fb2d54e70548db898
marovichn <nikolamarovic.11@gmail.com>
2024-08-29 21:29:29 +0200

    SMS Manager v1.0.0

--- main.py
+++ main.py
@@ -4,6 +4,6 @@ account_sid = "ACCID"
 auth_token = "TOKEN"
 client = Client(account_sid, auth_token)
 
-message = client.messages.create(from_='tw_number', to='input_number')
+message = client.messages.create(from_='tw_number', body='MESSAGE', to='input_number')
 
 print(message.sid)
